26f5a3ea47c6af2452e3ca4325540a0a680214ee
TopDevIK <inovichyakoblev0725@gmail.com>
2024-04-23 17:11:51 -0700

    vessels table component

---
+++ client-vessels/components/VesselsTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card>
+    <v-table :loading="props.loading">
+      <thead>
+        <tr>
+          <th class="text-left text-green">
+            Vessel Id
+          </th>
+          <th class="text-left text-green">
+            Position Time
+          </th>
+          <th class="text-left text-green">
+            Latitude
+          </th>
+          <th class="text-left text-green">
+            Longitude
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in vessels"
+          :key="item.id"
+        >                
+          <td>{{ item.vessel_id }}</td>
+          <td>
+            {{ $dayjs(item.position_time).format('YYYY/MM/DD HH:mm:ss') }}
+          </td>
+          <td>
+            <v-chip color="primary"> {{ item.latitude }}</v-chip>
+          </td>
+          <td>
+            <v-chip color="primary"> {{ item.longitude }}</v-chip>
+          </td>
+        </tr>
+      </tbody>
+      
+    </v-table>
+  </v-card>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  vessels: {
+    required: true,
+    default: []
+  },
+  loading: {
+    required: false,
+    default: false
+  }
+})
+</script>
